ae7f8dab1407809b3a83f6551e1a0fd9265321e2
Jose Guerrero <joseg047@ufl.edu>
2020-09-29 22:43:35 -0400

    added a parachute model

--- public/examples/Parachute/scripts.js
+++ public/examples/Parachute/scripts.js
@@ -1,7 +1,7 @@
 var scene = new THREE.Scene();
 
 var camera = new THREE.PerspectiveCamera( 75, window.innerWidth/window.innerHeight, 0.1, 1000 );
-camera.position.z = 200;
+camera.position.z =2;
 
 var renderer = new THREE.WebGLRenderer();
 renderer.setSize( window.innerWidth, window.innerHeight );
@@ -9,13 +9,13 @@ document.body.appendChild( renderer.domElement );
 
 var controls = new THREE.OrbitControls(camera, renderer.domElement);
 controls.enableDamping = true;
-controls.dampingFactor = 0.25;
+controls.dampingFactor = 0.5;
 controls.enableZoom = true;
 
-var keyLight = new THREE.DirectionalLight(new THREE.Color('hsl(0,0%,100%)'), 1.0);
+var keyLight = new THREE.DirectionalLight(new THREE.Color('hsl(30,0%,75%)'), 1.0);
 keyLight.position.set(-100, 0, 100);
 
-var fillLight = new THREE.DirectionalLight(new THREE.Color('hsl(240, 100%, 75%)'), 0.75);
+var fillLight = new THREE.DirectionalLight(new THREE.Color('hsl(240,100%,52%)'), 0.75);
 fillLight.position.set(100, 0, 100);
 
 var backLight = new THREE.DirectionalLight(0xffffff, 1.0);
@@ -26,31 +26,28 @@ scene.add(fillLight);
 scene.add(backLight);
 
 var mtlLoader = new THREE.MTLLoader();
-mtlLoader.setTexturePath('/examples/bottom/assets/');
-mtlLoader.setPath('/examples/bottom/assets/');
-mtlLoader.load('bottom.mtl', function (materials) {
+mtlLoader.setTexturePath('/examples/Parachute/assets/');
+mtlLoader.setPath('/examples/Parachute/assets/');
+mtlLoader.load('Parachute.mtl', function (materials) {
 
     materials.preload();
 
     var objLoader = new THREE.OBJLoader();
     objLoader.setMaterials(materials);
-    objLoader.setPath('/examples/bottom/assets/');
-    objLoader.load('bottom.obj', function (object) {
+    objLoader.setPath('/examples/Parachute/assets/');
+    objLoader.load('model.dae.obj', function (object) {
 
         scene.add(object);
-        object.position.y -= 60;
+        object.position.y -= 0;
 
     });
 
 });
 
 var animate = function () {
-
 	requestAnimationFrame( animate );
 	controls.update();
-
 	renderer.render(scene, camera);
-
 };
 
 animate();
